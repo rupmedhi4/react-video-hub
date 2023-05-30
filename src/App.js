5e5768bbbc4da278af82c4a589b000945957815e
Rup medhi <rupmmedhi@gmail.com>
2023-05-30 21:49:57 +0530

    video page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./Components/Header";
 import Home from './Components/Home';
 import Footer from './Components/Footer';
+import Videos from './Components/Videos';
+import Upload from './Components/Upload';
 function App() {
   return (
     <>
@@ -9,6 +11,8 @@ function App() {
      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/videos' element={<Videos/>}/>
+        <Route path='/upload' element={<Upload/>}/>
       </Routes>
       <Footer/>
     </Router>
